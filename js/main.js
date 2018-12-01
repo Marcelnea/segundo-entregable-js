0771bdfa07540ab5b3c006bd79c1ff8e0c17c591
MarcelaBustos <marcelabustos.contacto@gmail.com>
2018-11-30 23:44:07 -0300

    segundo commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+
+/*acciones galeria/proyectos.html*/
+
 $(document).ready(function(){
 	$('.enlaces').click(function(){
 		var valor = $(this).attr('data nombre');
@@ -11,4 +14,29 @@ $(document).ready(function(){
 			$(this).addclass('active');siblings().removeclass('activo');
 		});
 	});
+});
+
+/*acciones servicios.html*/
+
+$(document).ready(function(){
+
+	$(window).scroll(function(){
+		var barra = $(window).scrollTop();
+		var posicion =  (barra * 0.10);
+
+		$('body').css({
+			'background-position': '0 -' + posicion + 'px'
+		});
+	});
+});
+
+$(document).ready(function(){
+	var ancho = $(window).width();
+
+	if (ancho <= 1350){
+		$('body').css({
+			'background-size': 'initial'
+		});
+	}
+
 });
